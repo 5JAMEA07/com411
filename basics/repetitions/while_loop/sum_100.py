0770ca67cb313e2c15a39fb07823bda91b1c48c7
Abraham <geniusguy390@gmail.comgit config --global user.name Abrahamgit config --global user.email geniusguy390@gmail.com>
2021-10-13 23:08:17 +0100

    Added code to demonstrate the use of summing with a while loop.

---
+++ basics/repetitions/while_loop/sum_100.py
@@ -0,0 +1,10 @@
+print("Calculating the sum of the first 100 numbers...")
+
+counter = 1
+final = 0
+
+while counter <= 100:
+    final += sum
+    sum += 1
+
+print(f"...Done! The answer is {final}")
