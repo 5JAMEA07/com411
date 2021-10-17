f2e049469ded7e4566a05569445401fd7f181e27
Abraham <geniusguy390@gmail.comgit config --global user.name Abrahamgit config --global user.email geniusguy390@gmail.com>
2021-10-17 13:18:17 +0100

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,9 @@
+def identify():
+    sight = input("What lies before us?\n")
+    if sight == "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine.")
+
+
+identify()
