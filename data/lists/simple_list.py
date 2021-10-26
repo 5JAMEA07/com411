4b2a4339e39026690ce10c4005bf37f84a27754f
Abraham <geniusguy390@gmail.comgit config --global user.name Abrahamgit config --global user.email geniusguy390@gmail.com>
2021-10-26 23:40:03 +0100

    Added code to demonstrate a simple list.

---
+++ data/lists/simple_list.py
@@ -0,0 +1,12 @@
+def directions():
+    direction = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+    return direction
+
+
+def run():
+    data = directions()
+    print(data)
+
+
+if __name__ == "__main__":
+    run()
