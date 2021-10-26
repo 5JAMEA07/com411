4bd79976a55efbc011a66a01ca6061b95396f4ca
Abraham <geniusguy390@gmail.comgit config --global user.name Abrahamgit config --global user.email geniusguy390@gmail.com>
2021-10-26 23:28:15 +0100

    Added code to export JSON data to a file.

---
+++ data/files/json/export_json.py
@@ -0,0 +1,26 @@
+import json
+
+
+def read(file_path):
+    print("Reading...", end="")
+    with open(file_path) as file:
+        data = json.load(file)
+    print("Done!")
+
+    return data
+
+
+def save(file_path, data):
+    print("Exporting...", end="")
+    with open(file_path, "w") as file:
+        json.dump(data, file, indent=4)
+    print("Done!")
+
+
+def run():
+    data = read("robocity.json")
+    save("exported.json", data)
+
+
+if __name__ == "__main__":
+    run()
