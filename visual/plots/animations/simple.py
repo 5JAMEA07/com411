5a0ef5af8cbfc1e4c40b569be1f718ef5cf458c3
unknown <5jamea07@solent.ac.uk>
2021-12-16 15:26:26 +0000

    Added code to create basic human and robot classes.

---
+++ visual/plots/animations/simple.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+
+
+def animate(frame):
+    global ax
+    ax.set
+    # your code here (use ax to draw)
+
+
+def run():
+    global fig
+    # your code here (use fig with animation function)
+
+
+run()
